20d567b0fedfe8a8d38cc9f56c7ef24287c66bb2
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2017-08-08 11:34:51 -0400

    Imported JSON and deleted unused file

--- shapelytest.py
+++ shapelytest.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
+#---------------------------------- 
+#  Name: ExportCuration.py
+#---------------------------------- 
+
 import json
 import shapely
 
 cur_objects = json.loads(open('curations.json').read())
 
 print cur_objects[1]["geometry"]
+
